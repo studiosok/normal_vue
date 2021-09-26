e3a0b3d520c317c6d35b42aac08c8c3d022feba9
Kristen Studioso <45612650+studiosok@users.noreply.github.com>
2021-09-25 18:32:46 -0700

    In Process: setup

---
+++ src/Components/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Normal</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import defineComponent from 'vue';
+export default new defineComponent({
+    name: 'App',
+});
+</script>
---
+++ src/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import Vue from 'vue';
+import App from '../Components/App.vue';
+
+new Vue({
+    el: '#app',
+    // components: { App },
+    template: '<App/>',
+});
+</script>
